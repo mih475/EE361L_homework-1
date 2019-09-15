a369eabd80ca8f763f15cb0dde7ba0e71aa52c63
mih475 <mih475@utexas.edu>
2019-09-15 14:46:54 -0500

    working on layout

--- app/src/main/java/com/example/homework_1/MainActivity.java
+++ app/src/main/java/com/example/homework_1/MainActivity.java
@@ -3,19 +3,24 @@ package com.example.homework_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SearchView;
 
 import com.example.homework_1.ui.main.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    SearchView searchView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow();
         }
     }
+
 }
