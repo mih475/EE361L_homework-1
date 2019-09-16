3503cd35a30e8f2fc6aa5c86a3ed01c6f15640af
Miguel Garza <garzarobm@utexas.edu>
2019-09-15 22:52:21 -0500

    Got the Json parser to work

--- app/src/main/java/com/example/homework_1/MainActivity.java
+++ app/src/main/java/com/example/homework_1/MainActivity.java
@@ -2,14 +2,41 @@ package com.example.homework_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
 
 import com.example.homework_1.ui.main.MainFragment;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+
+
 public class MainActivity extends AppCompatActivity {
 
+    private String TAG = MainActivity.class.getSimpleName();
+    private ListView lv;
+
+    ArrayList<HashMap<String, String>> contactList;
+
     SearchView searchView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +48,127 @@ public class MainActivity extends AppCompatActivity {
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow();
         }
+        contactList = new ArrayList<>();
+        lv = (ListView) findViewById(R.id.list);
+        new GetContacts().execute();
+
+
     }
+
+    /**
+     * Show a toast
+     * @param view -- the view that is clicked
+     */
+    public void toastMe(View view){
+        // Toast myToast = Toast.makeText(this, message, duration);
+        Toast myToast = Toast.makeText(this, "Json Data is \n" +
+                        "            downloading",
+                Toast.LENGTH_SHORT);
+        myToast.show();
+
+
+
+
+    }
+
+    private class GetContacts extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+
+
+            HttpHandler sh = new HttpHandler();
+            // Making a request to url and getting response
+            String url = "https://api.androidhive.info/contacts/";
+            String jsonStr = sh.makeServiceCall(url);
+
+
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting JSON Array node
+                    JSONArray results = jsonObj.getJSONArray("results");
+
+                    //Getting the geometry
+                    JSONArray geometry = results.getJSONObject(1).getJSONArray("geometry");
+
+
+                    // looping through All Contacts
+                    for (int i = 0; i < geometry.length();i++){
+                        JSONObject c = results.getJSONObject(i);
+                        String id = c.getString("id");
+                        String name = c.getString("name");
+                        String email = c.getString("email");
+                        String address = c.getString("address");
+                        String gender = c.getString("gender");
+
+                        // Phone node is JSON Object
+                        JSONObject phone = c.getJSONObject("phone");
+                        String mobile = phone.getString("mobile");
+                        String home = phone.getString("home");
+                        String office = phone.getString("office");
+
+                        // tmp hash map for single contact
+                        HashMap<String, String> contact = new HashMap<>();
+
+                        // adding each child node to HashMap key => value
+                        contact.put("id", id);
+                        contact.put("name", name);
+                        contact.put("email", email);
+                        contact.put("mobile", mobile);
+
+                        // adding contact to contact list
+                        contactList.add(contact);
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                }
+
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            ListAdapter adapter = new SimpleAdapter(MainActivity.this, contactList,
+                    R.layout.list_item, new String[]{ "email","mobile"},
+                    new int[]{R.id.email, R.id.mobile});
+            lv.setAdapter(adapter);
+        }
+    }
+
+
+
+
 
 }
